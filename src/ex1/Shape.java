92e0809d6ac9ec8aa895c3c0b4cedaa8d5cfd5a8
ExxxxT <aleks9308@gmail.com>
2020-09-21 17:12:46 +0300

    Initial commit

---
+++ src/ex1/Shape.java
@@ -0,0 +1,13 @@
+package ex1;
+
+public abstract  class Shape
+{
+    class ExtendedByShaper1 extends Shape
+    {
+
+    }
+    class ExtendedByShaper2 extends Shape
+    {
+
+    }
+}
---
+++ src/ex5/MovablePoint.java
@@ -0,0 +1,8 @@
+package ex5;
+
+
+public class MovablePoint extends MovableRectangle implements Movable {
+    public MovablePoint(int x1, int y1, int x2, int y2) {
+        super(x1, y1, x2, y2);
+    }
+}
